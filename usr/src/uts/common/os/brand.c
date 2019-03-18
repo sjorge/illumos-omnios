e28e442af8aadc4a434ceb57d61aa28929dc7c0b
Patrick Mooney <pmooney@pfmooney.com>
2019-03-18 15:28:27 +0000

    14236 signed math leads getelfshdr astray (re-sync)

--- usr/src/uts/common/os/brand.c
+++ usr/src/uts/common/os/brand.c
@@ -671,9 +671,9 @@ restoreexecenv(struct execenv *ep, stack_t *sp)
 /*ARGSUSED*/
 int
 brand_solaris_elfexec(vnode_t *vp, execa_t *uap, uarg_t *args,
-    intpdata_t *idatap, int level, size_t *execsz, int setid,
-    caddr_t exec_file, cred_t *cred, int *brand_action, struct brand *pbrand,
-    char *bname, char *brandlib, char *brandlib32)
+    intpdata_t *idatap, int level, size_t *execsz, int setid, caddr_t exec_file,
+    cred_t *cred, int *brand_action, struct brand *pbrand, char *bname,
+    char *brandlib, char *brandlib32)
 {
 
 	vnode_t		*nvp;
