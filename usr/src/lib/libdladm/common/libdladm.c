89f1b10cd68f65f5b587ee647b66488aa069dbe0
Andy Fiddaman <omnios@citrus-it.co.uk>
2022-02-23 16:02:58 +0000

    13500 Want support for overlay networks (fix mismerge)

--- usr/src/lib/libdladm/common/libdladm.c
+++ usr/src/lib/libdladm/common/libdladm.c
@@ -441,9 +441,6 @@ dladm_status2str(dladm_status_t status, char *buf)
 	case DLADM_STATUS_INVALID_MTU:
 		s = "MTU check failed, MTU outside of device's supported range";
 		break;
-	case DLADM_STATUS_BAD_ENCAP:
-		s = "invalid encapsulation protocol";
-		break;
 	case DLADM_STATUS_ADDRNOTAVAIL:
 		s = "can't assign requested address";
 		break;
